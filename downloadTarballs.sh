7be6b73f0cfe87882a17099add821c37149a6f21
dev-otl <102328917+dev-otl@users.noreply.github.com>
2024-04-05 19:14:02 +0200

    Create downloadTarballs.sh
    add script to download necessary tarballs

---
+++ downloadTarballs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -xe
+
+TARBALLS=(\
+    binutils-2.24+os161-2.1.tar.gz \
+    gcc-4.8.3+os161-2.1.tar.gz \
+    gdb-7.8+os161-2.1.tar.gz \
+    os161-base-2.0.3.tar.gz \
+    sys161-2.0.8.tar.gz \
+)
+BASELINK=http://os161.org/download/
+
+for tarball in ${TARBALLS[@]}
+    do
+    if [ -f $tarball ]
+        then echo "removing existing $tarball";
+             rm $tarball;
+    fi
+    echo downloading $tarball;
+    wget $BASELINK$tarball;
+done
+
+exit
